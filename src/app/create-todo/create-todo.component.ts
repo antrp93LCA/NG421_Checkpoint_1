d74261f9f72e9f42f81584b63aee90d74e67c374
Anthony Perez <antrp93@gmail.com>
2020-04-16 20:11:37 -0500

    default properties for a new TodoItem set

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {TodoService} from '../services/todo.service';
+import { ITodo } from '../interfaces/itodo';
 
 @Component({
   selector: 'app-create-todo',
@@ -7,16 +8,22 @@ import {TodoService} from '../services/todo.service';
   styleUrls: ['./create-todo.component.css']
 })
 export class CreateTodoComponent implements OnInit {
-  todoTitle = ''
+
+  todo :ITodo
+  
+  
   constructor(private todoService : TodoService) { }
 
   ngOnInit() {
   }
+
   addTodo(ITodo):void {
     this.todoService.addTodo(ITodo);
     
     // resets our todoTitle variable to an empty string
-    this.todoTitle = '';
+    this.todo.title = '';
+    this.todo.status = 'Todo';
+    this.todo.createdAt = new Date();
   }
 
 }
